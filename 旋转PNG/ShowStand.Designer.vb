5cd7fa7a05dbdc29c566342a93a617fc4446b5b4
Leon <xiaoyankeji@hotmail.com>
2016-08-07 19:25:38 +0800

    第一次提交

---
+++ 旋转PNG/ShowStand.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ShowStand
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RotateShow = New System.Windows.Forms.PictureBox()
+        CType(Me.RotateShow, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RotateShow
+        '
+        Me.RotateShow.BackColor = System.Drawing.Color.Transparent
+        Me.RotateShow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RotateShow.Location = New System.Drawing.Point(0, 0)
+        Me.RotateShow.Name = "RotateShow"
+        Me.RotateShow.Size = New System.Drawing.Size(360, 360)
+        Me.RotateShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.RotateShow.TabIndex = 0
+        Me.RotateShow.TabStop = False
+        '
+        'ShowStand
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImage = Global.旋转PNG.My.Resources.PNGResource.背景
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(360, 360)
+        Me.Controls.Add(Me.RotateShow)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "ShowStand"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ShowStand"
+        Me.TopMost = True
+        CType(Me.RotateShow, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RotateShow As System.Windows.Forms.PictureBox
+
+End Class
---
+++ 旋转PNG/ShowStand.vb
@@ -0,0 +1,88 @@
+﻿Option Explicit On
+'呵呵哒
+Imports System.Drawing.Drawing2D
+
+Public Class ShowStand
+    '让鼠标拖动无边框窗体使用的API
+    Private Declare Function ReleaseCapture Lib "user32" () As Integer
+    Private Declare Function SendMessageA Lib "user32" (ByVal hwnd As Integer, ByVal wMsg As Integer,
+                                                                            ByVal wParam As Integer, lParam As VariantType) As Integer
+
+    '旋转位图专用的线程，我嫌Time控件麻烦（其实多线程才更麻烦好吧！！！）
+    Dim RotateThread As Threading.Thread
+
+    '程序启动
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '一些UI设计，不要在意
+        Dim MyBackground As Bitmap = My.Resources.PNGResource.背景
+        Dim MyGraphics As Graphics = Graphics.FromImage(MyBackground)
+        MyGraphics.SmoothingMode = SmoothingMode.HighQuality
+        MyGraphics.PixelOffsetMode = PixelOffsetMode.HighQuality
+        Dim MyBitmap As Bitmap = My.Resources.PNGResource.瞄准准星
+        MyGraphics.DrawImage(MyBitmap, (Me.Width - MyBitmap.Width) \ 2, (Me.Height - MyBitmap.Height) \ 2)
+        Me.BackgroundImage = MyBackground
+        MyGraphics.Dispose()
+    End Sub
+
+    Private Sub ShowStand_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        '启动旋转位图的线程，函数地址：ShowRotateBitmap
+        RotateThread = New Threading.Thread(AddressOf ShowRotateBitmap)
+        RotateThread.Start()
+    End Sub
+
+    '程序退出
+    Private Sub ShowStand_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        '窗体关闭时，中断多线程
+        RotateThread.Abort()
+        '卸载线程
+        RotateThread = Nothing
+    End Sub
+
+    '多线程绑定的函数：
+    Private Sub ShowRotateBitmap()
+        '无限循环，不停的旋转
+        Do While True
+            '静态变量：旋转角度
+            Static Angle As Single = 0.0
+            '线程睡眠10微秒，防止占用过多CPU
+            Threading.Thread.Sleep(10)
+            '调用函数
+            RotateShow.Image = GetRotateBitmap(My.Resources.PNGResource.旋转进度条, Angle)
+            RotateShow.BackgroundImage = GetRotateBitmap(My.Resources.PNGResource.瞄准准星, Angle * 5)
+            '角度循环加大
+            Angle = IIf(Angle = 360, 0, Angle + 1)
+        Loop
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles _
+                                                                            Me.MouseDown, RotateShow.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Right Then Me.Close() : End
+        '鼠标拖动窗体
+        ReleaseCapture()
+        SendMessageA(Me.Handle, &HA1, 2, 0&)
+    End Sub
+
+    ''' <summary>
+    ''' 返回旋转任意角度后的图像
+    ''' </summary>
+    ''' <param name="BitmapRes">需要旋转处理的图像</param>
+    ''' <param name="Angle">旋转角度[单位：度]</param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function GetRotateBitmap(ByVal BitmapRes As Bitmap, ByVal Angle As Single) As Bitmap
+        Dim ReturnBitmap As New Bitmap(BitmapRes.Width, BitmapRes.Height)
+        Dim MyGraphics As Graphics = Graphics.FromImage(ReturnBitmap)
+        MyGraphics.SmoothingMode = SmoothingMode.HighQuality
+        MyGraphics.PixelOffsetMode = PixelOffsetMode.HighQuality
+
+        MyGraphics.TranslateTransform(BitmapRes.Width / 2, BitmapRes.Height / 2)
+        MyGraphics.RotateTransform(Angle, MatrixOrder.Prepend)
+
+        MyGraphics.TranslateTransform(-BitmapRes.Width / 2, -BitmapRes.Height / 2)
+        MyGraphics.DrawImage(BitmapRes, 0, 0, BitmapRes.Width, BitmapRes.Height)
+        MyGraphics.Dispose()
+
+        Return ReturnBitmap
+    End Function
+
+End Class
